a620ff5570d2bb8b144c575690bbb922ece96c3c
majerm <majerm@educ.college-montreal.qc.ca>
2022-10-07 15:27:10 -0400

    TP2 finalisation du code avec l'installation de la fonction qui compte le nombre d'essaies

--- TP2.py
+++ TP2.py
@@ -4,23 +4,30 @@ Groupe: 403
 Jeu de devinette avec chiffre aléatoire
 """
 from random import *
-chiffre_recherche = randint(0, 10)
 
 boucle_jeu = True
+
+
 while boucle_jeu:
-    question_chiffre = int(input("J'ai choisi un chiffre entre 0 et 10. À vous de le deviner."))
-    if question_chiffre > chiffre_recherche:
-        print("Incorrecte, le chiffre est plus petit.")
-        boucle_jeu = True
-    elif question_chiffre < chiffre_recherche:
-        print("Incorrecte, le chiffre est plus grand.")
-        boucle_jeu = True
-    elif question_chiffre == chiffre_recherche:
-        question_fin_jeu = str(input("Vous avez bien deviné le chiffre. Voulez vous rejouer?"))
-        if question_fin_jeu == "oui":
-            boucle_jeu = True
-        elif question_fin_jeu == "non":
-            boucle_jeu = False
-        else:
-            boucle_jeu = True
+    chiffre_recherche = randint(0, 10)
+    nb_essaie = 1
+
+    not_found = True
+    while not_found:
+        question_chiffre = int(input("J'ai choisi un chiffre entre 0 et 10. À vous de le deviner."))
+        if question_chiffre > chiffre_recherche:
+            print("Incorrecte, le chiffre est plus petit.")
+            nb_essaie += 1
+
+        elif question_chiffre < chiffre_recherche:
+            print("Incorrecte, le chiffre est plus grand.")
+            nb_essaie += 1
+
+        elif question_chiffre == chiffre_recherche:
+            question_fin_jeu = str(input(f"Vous avez bien deviné le chiffre en {nb_essaie} essaies. Voulez vous rejouer?"))
+            if question_fin_jeu == "oui":
+                boucle_jeu = True
+                not_found = False
+            elif question_fin_jeu == "non":
+                not_found = False
 
